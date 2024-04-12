0a5f82b969e25c1c71d23d84522b80ed74137b27
karansingh <Karanbir97@outlook.com>
2024-04-11 21:46:12 -0500

    Made the model for SuperHero

---
+++ SuperHeroAPIDotNet7/Controllers/SuperHeroController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPIDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllHeroes()
+        {
+            var 
+        }
+    }
+}
